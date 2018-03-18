68ef0b79f6c1373288b6b204f4aa3335d4cf625a
Kyle Bradford <kfoxb@kfoxb.com>
2018-03-18 15:22:01 -0600

    (feat) used styled-components to center login button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { withRouter } from 'react-router';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 function App({ handleAuth, isAuthenticated }) {
-  return (<button onClick={handleAuth}>{isAuthenticated ? 'Logout' : 'Login'}</button>);
+  const FlexContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh
+  `;
+  return (
+    <FlexContainer>
+      <button onClick={handleAuth}>{isAuthenticated ? 'Logout' : 'Login'}</button>
+    </FlexContainer>
+  );
 }
 
 App.propTypes = {
